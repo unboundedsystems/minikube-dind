7e6ded71ccb48ab14c50f16595c0dc669e232c21
Mark Terrel <mark@unbounded.systems>
2018-09-28 13:55:33 -0600

    Don't run /dindnet The attempt to run it is extraneous, at least for the current version(s) of minikube. The file doesn't exist, so it always errors.

--- start.sh
+++ start.sh
@@ -39,7 +39,6 @@ tail -F /var/log/docker.log /var/log/dind.log /var/log/minikube-start.log &
 child=$!
 
 export CNI_BRIDGE_NETWORK_OFFSET="0.0.1.0"
-/dindnet &> /var/log/dind.log 2>&1 < /dev/null &
 
 dockerd \
   --host=unix:///var/run/docker.sock \
